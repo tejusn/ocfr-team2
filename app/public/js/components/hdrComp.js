f0f43b586468499939c29d08280e57dbc76f4aa7
tejusn <successtej@gmail.com>
2019-10-10 17:27:24 -0400

    changes to milestone

--- app/public/js/components/hdrComp.js
+++ app/public/js/components/hdrComp.js
@@ -2,20 +2,20 @@ Vue.component('page-header', {
   	template: `<header>
                 <!-- Fixed navbar -->
                 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                  <a class="navbar-brand" href="#">OCONEE COUNTY</a>
+                  <a class="navbar-brand" href="index.html">OCONEE COUNTY</a>
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav mr-auto">
-                      <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="html/certs.html">Certifications</a>
+                        <a class="nav-link" href="certs.html">Certifications</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Members</a>
+                        <a class="nav-link" href="members.html" tabindex="-1" aria-disabled="true">Members</a>
                       </li>
                     </ul>
 

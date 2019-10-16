bfc312635f089d705a7d3e4c9f9af0f32dda9015
mbbrici <54514457+mbbrici@users.noreply.github.com>
2019-10-16 14:01:55 -0400

    attempt post.php DELETE MEMBER

---
+++ app/public/api/records/post.php
@@ -0,0 +1,48 @@
+<?php
+
+// Step 0: Validate data
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Prepare & run the query
+$stmt = $db->prepare(
+  'DELETE FROM Member WHERE id=1
+);
+
+$stmt->execute([
+  $_DELETE['patientGuid'],
+  $_DELETE['visitDescription'],
+  $_DELETE['priority']
+]);
+
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
+header('Location: ../waiting/');
+
+//
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to delete a record
+$sql = "DELETE FROM MyGuests WHERE id=3";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>

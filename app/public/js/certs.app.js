963e312c7929f280a1696e2246bd668dacbf16c4
mbbrici <54514457+mbbrici@users.noreply.github.com>
2019-10-17 19:51:53 -0400

    addCertification.php, updateCertification.php,certs.html completed, shows in html preview, will not display form and table in localhost:8080

--- app/public/js/certs.app.js
+++ app/public/js/certs.app.js
@@ -2,6 +2,8 @@ var certsRecordsApp = new Vue({
   el: '#certsRecordsApp',
   data: {
     certs: [],
+    recordCert: {},
+    mode:"add" //mode can be add or update
   },
   methods: {
     fetchCerts() {
@@ -9,13 +11,53 @@ var certsRecordsApp = new Vue({
       .then(response => response.json())
       .then(json => { certsRecordsApp.certs = json })
     },
-      // handleSubmit(event) {},
-      // handleReset() {},
-      // handleRowClick() {
-      //   // memberRecordsApp.member = member;
-      // }
+    handleSubmit(event) {
+      if(this.mode === "add"){ // add certification logic
+        fetch('api/records/addCertification.php', {
+          method:'POST',
+          body: JSON.stringify(this.recordCertification),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => { certsRecordsApp.certs.push(json[0])})
+        .catch( err => {
+          console.error('Cert Add POST ERROR:');
+          console.error(err);
+        })
+      }
+      else{ //update certification logic
+        fetch('api/records/updateCertification.php', {
+          method:'POST',
+          body: JSON.stringify(this.recordCert),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => { certsRecordsApp.certs.push(json[0]) })
+        .catch( err => {
+          console.error('Cert Update POST ERROR:');
+          console.error(err);
+        })
+        this.mode = "add";
+      }
 
-  },
+      this.handleReset();
+    },
+    handleReset() {
+      this.recordCert = {}
+    },
+    updateCert(cert){
+      // console.log(cert);
+      this.recordCert = cert;
+      this.mode = "update";
+    },
+    handleRowClick(certification) {
+      // certsRecordsApp.cert = cert;
+    }
+    }, // end methods
   created() {
     this.fetchCerts();
   }

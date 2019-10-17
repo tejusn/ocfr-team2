b5923e58be65a52c0a73890ae384aee6c98ffa9a
Amy Liu <amyliu@iu.edu>
2019-10-17 16:35:56 -0400

    changes on members expired certs pages

--- app/public/api/reports/membersExpCerts.php
+++ app/public/api/reports/membersExpCerts.php
@@ -4,7 +4,11 @@
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$stmt = $db->prepare('SELECT * FROM Member');
+$stmt = $db->prepare ('SELECT Member.member_id, Member.first_name, Member.last_name, Certification.certification_name, Certification.default_exp_period
+  FROM Member
+  INNER JOIN CertificationStatus on Member.member_id = CertificationStatus.member_id
+  INNER JOIN Certification on Certification.certification_id = CertificationStatus.certification_id ');
+
 $stmt->execute();
 $membersExpCerts = $stmt->fetchAll();
 
